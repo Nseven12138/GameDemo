4b179fe3879f9d2e6fa20473f8e64b64e27996ee
gaojianning@anji-tec.com <gaojianning@anji-tec.com>
2019-06-10 14:59:05 +0800

    First Commit

---
+++ The Kingdom of Nine/Bean.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace The_Kingdom_of_Nine
+{
+    class Bean:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;                   //属性改变时触发的事件
+        private string hP;
+        private string dPS;
+        private string lEVEL;
+        private string mhP;
+        private string mdPS;
+        private string mlEVEL;
+
+        public string HP                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.hP;
+            }
+            set
+            {
+                if (this.hP != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.hP = value;                                          //更新文件名
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("HP"));
+                    }
+                }
+            }
+        }
+        public string DPS                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.dPS;
+            }
+            set
+            {
+                if (this.dPS != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.dPS = value;                                          //更新文件名
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("DPS"));
+                    }
+                }
+            }
+        }
+        public string LEVEL                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.lEVEL;
+            }
+            set
+            {
+                if (this.lEVEL != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.lEVEL = value;                                      
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("LEVEL"));
+                    }
+                }
+            }
+        }
+        public string MHP                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.mhP;
+            }
+            set
+            {
+                if (this.mhP != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.mhP = value;                                          //更新文件名
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("MHP"));
+                    }
+                }
+            }
+        }
+        public string MDPS                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.mdPS;
+            }
+            set
+            {
+                if (this.mdPS != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.mdPS = value;                                          //更新文件名
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("MDPS"));
+                    }
+                }
+            }
+        }
+        public string MLEVEL                                                      //实际中用到的变量名
+        {
+            get
+            {
+                return this.mlEVEL;
+            }
+            set
+            {
+                if (this.mlEVEL != value)           //如果当前的变量值不等于先前的文件名，说明需要更新文件名
+                {
+                    this.mlEVEL = value;
+                    if (PropertyChanged != null)                                    //如果已经触发了改变事件
+                    {
+                        //通知绑定此变量的textbox在前台更新
+                        PropertyChanged(this, new PropertyChangedEventArgs("MLEVEL"));
+                    }
+                }
+            }
+        }
+
+    }
+}
--- The Kingdom of Nine/MainWindow.xaml.cs
+++ The Kingdom of Nine/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace The_Kingdom_of_Nine
         public MainWindow()
         {
             InitializeComponent();
+            this.KeyDown += Window_KeyDown;               
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        { }
+        //Esc for close 
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            { this.Close(); }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_ClickExit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_ClickStart(object sender, RoutedEventArgs e)
+        {
+            StartWindow st = new StartWindow();
+            st.Show();
+            this.Close();
+         
         }
     }
 }
